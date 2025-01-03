0825babeb5b3653c21b4da847307a2f20988b1e5
PamanCap <abiyyuzahy02@gmail.com>
2025-01-04 04:53:16 +0700

    Update dashboard.py

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 from babel.numbers import format_currency
+sns.set(style='white')
 
 day_df = pd.read_csv("Dashboard/day.csv")
 
